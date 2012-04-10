de66c329a46393ca022e0ec0e549baa15a599580
juacompe <juacompe@gmail.com>
2012-04-10 16:53:16 +0700

    - add pg_buildbot settnigs

---
+++ goofy_project/settings/pg_buildbot.py
@@ -0,0 +1,13 @@
+from goofy_project.settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.psycopg2',
+        'NAME': 'goofy',
+        'USER': 'goofyuser',
+        'PASSWORD': 'goofypass',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
