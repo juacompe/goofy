a1dcbe9e9768bf45b3200506f0a72cc42d4f170f
juacompe <juacompe@gmail.com>
2012-04-10 15:16:05 +0700

    complete master config

--- buildbot_config/settings.py
+++ buildbot_config/settings.py
@@ -1,6 +1,6 @@
 BRANCH = "develop"
-MASTER_HOST = "buildbot_master_host"
-PROJECT_CODE_URL = "project_dirname"
+MASTER_HOST = "ec2-50-18-27-15.us-west-1.compute.amazonaws.com"
+PROJECT_CODE_URL = "src"
 PROJECT_NAME = "goofy"
-REPOSITORY_URL = "/path/to/repo"
+REPOSITORY_URL = "git://github.com/juacompe/goofy.git"
 
