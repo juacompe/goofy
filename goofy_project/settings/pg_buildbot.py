05e3e897f9fe3640b11dec7d634a67eae3ae8ea8
juacompe <juacompe@gmail.com>
2012-04-10 17:03:06 +0700

    - fix wrong database ENGINE

--- goofy_project/settings/pg_buildbot.py
+++ goofy_project/settings/pg_buildbot.py
@@ -2,7 +2,7 @@ from goofy_project.settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'goofy',
         'USER': 'goofyuser',
         'PASSWORD': 'goofypass',
